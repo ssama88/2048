e03171df503c56503b4c5a4c10fb5442d756ade2
Saket Sama <ssama@777part.com>
2024-07-25 13:24:07 -0700

    Draw main menu

---
+++ main.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 640  // window height
+#define SCREEN_HEIGHT 480 // window widt
+
+// surfaces
+static SDL_Surface *screen;
+static SDL_Surface *title;
+static SDL_Surface *numbermap;
+static SDL_Surface *end;
+
+int width, height; // used if fullscreen
+
+SDL_Window *window = NULL; // The window we'll be rendering to
+SDL_Renderer *renderer;    // The renderer SDL will use to draw to the screen
+
+// textures
+SDL_Texture *screen_texture;
+static void draw_menu()
+{
+    SDL_Rect src;
+    SDL_Rect dest;
+
+    src.x = 0;
+    src.y = 0;
+    src.w = title->w;
+    src.h = title->h;
+
+    dest.x = (screen->w / 2) - (src.w / 2);
+    dest.y = (screen->h / 2) - (src.h / 2);
+    dest.w = title->w;
+    dest.h = title->h;
+
+    SDL_BlitSurface(title, &src, screen, &dest);
+}
+int init(int width, int height, int argc, char *args[])
+{
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+
+        return 1;
+    }
+
+    int i;
+
+    for (i = 0; i < argc; i++)
+    {
+
+        // Create window
+        if (strcmp(args[i], "-f"))
+        {
+
+            SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN, &window, &renderer);
+        }
+        else
+        {
+
+            SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP, &window, &renderer);
+        }
+    }
+
+    if (window == NULL)
+    {
+
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+
+        return 1;
+    }
+
+    // create the screen sruface where all the elemnts will be drawn onto (ball, paddles, net etc)
+    screen = SDL_CreateRGBSurfaceWithFormat(0, width, height, 32, SDL_PIXELFORMAT_RGBA32);
+
+    if (screen == NULL)
+    {
+
+        printf("Could not create the screen surfce! SDL_Error: %s\n", SDL_GetError());
+
+        return 1;
+    }
+
+    // create the screen texture to render the screen surface to the actual display
+    screen_texture = SDL_CreateTextureFromSurface(renderer, screen);
+
+    if (screen_texture == NULL)
+    {
+
+        printf("Could not create the screen_texture! SDL_Error: %s\n", SDL_GetError());
+
+        return 1;
+    }
+
+    // Load the title image
+    title = SDL_LoadBMP("title.bmp");
+
+    if (title == NULL)
+    {
+
+        printf("Could not Load title image! SDL_Error: %s\n", SDL_GetError());
+
+        return 1;
+    }
+
+    // Load the numbermap image
+    numbermap = SDL_LoadBMP("numbermap.bmp");
+
+    if (numbermap == NULL)
+    {
+
+        printf("Could not Load numbermap image! SDL_Error: %s\n", SDL_GetError());
+
+        return 1;
+    }
+
+    // Load the gameover image
+    end = SDL_LoadBMP("gameover.bmp");
+
+    if (end == NULL)
+    {
+
+        printf("Could not Load title image! SDL_Error: %s\n", SDL_GetError());
+
+        return 1;
+    }
+
+    // Set the title colourkey.
+    Uint32 colorkey = SDL_MapRGB(title->format, 255, 0, 255);
+    SDL_SetColorKey(title, SDL_TRUE, colorkey);
+    SDL_SetColorKey(numbermap, SDL_TRUE, colorkey);
+
+    return 0;
+}
+int main(int argc, char *args[])
+{
+    if (init(SCREEN_WIDTH, SCREEN_HEIGHT, argc, args) == 1)
+    {
+
+        return 0;
+    }
+    SDL_GetWindowSize(window, &width, &height);
+
+    int sleep = 0;
+    int quit = 0;
+    int state = 0;
+    int r = 0;
+    Uint32 next_game_tick = SDL_GetTicks();
+
+    SDL_PumpEvents();
+
+    draw_menu();
+
+    SDL_UpdateTexture(screen_texture, NULL, screen->pixels, screen->w * sizeof(Uint32));
+    SDL_RenderCopy(renderer, screen_texture, NULL, NULL);
+
+    // draw to the display
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(10000);
+    // free loaded images
+    SDL_FreeSurface(screen);
+    SDL_FreeSurface(title);
+    SDL_FreeSurface(numbermap);
+    SDL_FreeSurface(end);
+
+    // free renderer and all textures used with it
+    SDL_DestroyRenderer(renderer);
+
+    // Destroy window
+    SDL_DestroyWindow(window);
+
+    // Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
+}
